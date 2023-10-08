82f7c3694c132bb4e8020c5825ced2d00af31a1b
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-08 19:48:14 -0300

    u

--- src/db.js
+++ src/db.js
@@ -20,6 +20,10 @@ let sequelize =
          logging: false, // set to console.log to see the raw SQL queries
          native: false, // lets Sequelize know we can use pg-native for ~30% more speed
        });
+       
+
+
+       /////
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/eco`, {
 //   logging: false, // set to console.log to see the raw SQL queries
