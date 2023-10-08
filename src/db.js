6e1f6141f6dd5eb252bbf3af1a16fb101a6bac8a
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-08 20:32:21 -0300

    f

--- src/db.js
+++ src/db.js
@@ -6,39 +6,54 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME,
 } = process.env;
 
-let sequelize;
 
-if (process.env.NODE_ENV === "production") {
-  sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-    {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    }
-  );
-} else {
-  sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  });
-}
+// let sequelize =
+//   process.env.NODE_ENV === "production"
+//     ? new Sequelize(
+//         `postgres://eco:tLKoFub6eYwIxpm4pY6NOSoYDYHWJaWs@dpg-ck7jn5g8elhc7388tm3g-a.oregon-postgres.render.com/ecologico`, //eso es lo que da railway en connect Postgres Connection URL
+//         {
+//           logging: false, // set to console.log to see the raw SQL queries
+//           native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//         }
+//       )
+//     : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/eco`, {
+//          logging: false, // set to console.log to see the raw SQL queries
+//          native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//        });
+
+
+//        /////
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/eco`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
+
 
 const basename = path.basename(__filename);
+
 const modelDefiners = [];
 
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, '/models'))
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
+// Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach(model => model(sequelize));
-
+// Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
+// En sequelize.models están todos los modelos importados como propiedades
+// Para relacionarlos hacemos un destructuring
 const { Products, Line, Brand, User, Review } = sequelize.models;
+
+// Aca vendrian las relaciones
+// Product.hasMany(Reviews);
 
 Products.belongsToMany(Line, { through: 'Products_Line' });
 Line.belongsToMany(Products, { through: 'Products_Line' });
@@ -53,6 +68,7 @@ User.hasMany(Review);
 Review.belongsTo(User);
 
 module.exports = {
-  ...sequelize.models,
-  conn: sequelize,
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
+
