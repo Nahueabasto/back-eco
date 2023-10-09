7f19b142d59cfc1d389dd18b80d28d02b148fa98
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-09 18:18:01 -0300

    che

--- src/db.js
+++ src/db.js
@@ -24,7 +24,7 @@ const {
 
 //        /////
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/eco`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
