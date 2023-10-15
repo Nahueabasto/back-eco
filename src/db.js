4a3f78d249e14e8e967c211297e98c6589c506d9
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-15 17:14:11 -0300

    sd

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DATABASE_URL,
 } = process.env;
 
 
@@ -25,7 +25,7 @@ const {
 
 //        /////
 
-const sequelize = new Sequelize(`postgres://eco:tLKoFub6eYwIxpm4pY6NOSoYDYHWJaWs@dpg-ck7jn5g8elhc7388tm3g-a.oregon-postgres.render.com/ecologico`, {
+const sequelize = new Sequelize(`:${DATABASE_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
