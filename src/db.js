8936280617636ffc81e610d687be75acb9e44492
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-16 15:47:01 -0300

    fs

--- src/db.js
+++ src/db.js
@@ -24,7 +24,7 @@ const {
 
 
 //        /////
-
+// postgres://USER:PASSWORD@INTERNAL_HOST:PORT/DATABASE
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
