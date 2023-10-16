b8bd30f996709dc68cf04835451dde63974d6093
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-16 17:03:45 -0300

    sd

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const express = require("express");
 const app = express();
 const { conn } = require('./src/db.js');
 const { Pool } = require("pg");
+require('dotenv').config();
+const server = require('./src/app.js');
 
+//config()
 
-// Crear un pool de PostgreSQL
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true
-// });
+//Crear un pool de PostgreSQL
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // ssl: true
+});
+
+server.get('/', async (req, res) => {
+  try {
+    // Utiliza el pool para ejecutar una consulta en PostgreSQL
+    const result = await pool.query('SELECT NOW()');
+
+    if (result.rows && result.rows.length > 0) {
+      res.json(result.rows[0]); // Enviar el resultado de la consulta como JSON
+    } else {
+      res.status(404).json({ error: 'No se encontraron resultados' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al ejecutar la consulta en la base de datos' });
+  }
+});
 
 //Define tus rutas aquí
 // app.get('/ping', async (req, res) => {
@@ -17,8 +37,6 @@ const { Pool } = require("pg");
 //     const result = await pool.query('SELECT NOW()');
 //     res.send(result.rows[0]); // Enviar el resultado de la consulta
 // });
-
-
 
 
 // Sincronizar todos los modelos a la vez (asumiendo que estás usando Sequelize)
@@ -30,10 +48,8 @@ const { Pool } = require("pg");
 //   });
 // });
 
-const server = require('./src/app.js');
-require('dotenv').config();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5432;
 
 server.listen(port, () => {
   console.log(`port runing in http://localhost:${port}`)
