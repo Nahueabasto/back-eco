15fa21ba48abccb1285f7783fc1dd4f5becb998c
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-17 15:43:19 -0300

    dsd

--- src/db-deploy.js
+++ src/db-deploy.js
@@ -3,7 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL_URL + "?sslmode=require",
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
 
 pool.connect((err) => {
